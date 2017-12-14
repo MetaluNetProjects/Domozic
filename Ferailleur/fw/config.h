066ec3eb0422aa342b6e6607db3bd0244c5c0511
Antoine Rousseau <_antoine_@metalu.net>
2017-12-14 17:58:18 +0100

    add Ferrailleur WIP

---
+++ Ferailleur/fw/config.h
@@ -0,0 +1,8 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define ANALOG_FILTER 4 
+#define ANALOG_THRESHOLD 10
+
+#endif // _CONFIG_H_
+
---
+++ Ferailleur/fw/main.c
@@ -0,0 +1,88 @@
+/*********************************************************************
+ *               analog example for Versa1.0
+ *	Analog capture on connectors K1, K2, K3 and K5. 
+ *********************************************************************/
+
+#define BOARD Versa1
+
+#include <fruit.h>
+#include <analog.h>
+#include <switch.h>
+
+t_delay mainDelay;
+
+/*#define LED0 K4
+#define LED1 K11
+#define LED2 K12*/
+
+void setup(void) {	
+//----------- Setup ----------------
+	fruitInit();
+			
+	
+	pinModeDigitalOut(LED); 	// set the LED pin mode to digital out
+	delayStart(mainDelay, 5000); 	// init the mainDelay to 5 ms
+
+	/*pinModeDigitalOut(LED0); 	// set the LED pin mode to digital out
+	digitalClear(LED0);
+	pinModeDigitalOut(LED1); 	// set the LED pin mode to digital out
+	digitalClear(LED1);
+	pinModeDigitalOut(LED2); 	// set the LED pin mode to digital out
+	digitalClear(LED2);*/
+
+//----------- Analog setup ----------------
+	analogInit();		// init analog module
+
+	switchInit();
+	INTCON2bits.RBPU = 0; // enable pullups on PORTB
+	
+	switchSelect(0,K10);	// assign connector K1 to analog channel 0
+	switchSelect(1,K9);
+	switchSelect(2,MB2);
+	switchSelect(3,MBEN2);
+	switchSelect(4,MBEN);
+	switchSelect(5,MB1);
+	switchSelect(6,K1);
+	switchSelect(7,K2);
+	switchSelect(8,K3);
+
+}
+
+void loop() {
+// ---------- Main loop ------------
+	fraiseService();	// listen to Fraise events
+	analogService();	// analog management routine
+	switchService();	// 
+	if(delayFinished(mainDelay)) // when mainDelay triggers :
+	{
+		delayStart(mainDelay, 5000); 	// re-init mainDelay
+		if(!switchSend()) analogSend();		// send analog channels that changed
+	}
+}
+
+// Receiving
+
+void fraiseReceiveChar() // receive text
+{
+	unsigned char c,c2;
+	
+	c=fraiseGetChar();
+	if(c=='L'){		//switch LED on/off 
+		c=fraiseGetChar();
+		digitalWrite(LED, c!='0');		
+	}
+	/*else if(c=='l'){		//switch LED on/off 
+		c=fraiseGetChar();
+		c2=fraiseGetChar();
+		if(c == '0') digitalWrite(LED0, c2 != '0');		
+		else if(c == '1') digitalWrite(LED1, c2 != '0');		
+		else if(c == '2') digitalWrite(LED2, c2 != '0');		
+	}*/
+	else if(c=='E') { 	// echo text (send it back to host)
+		printf("C");
+		c = fraiseGetLen(); 			// get length of current packet
+		while(c--) printf("%c",fraiseGetChar());// send each received byte
+		putchar('\n');				// end of line
+	}	
+}
+
