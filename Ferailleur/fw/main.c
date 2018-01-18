d8abf05deeed5ee79f15ab915474dfca0852d130
Antoine Rousseau <_antoine_@metalu.net>
2018-01-18 18:26:42 +0100

    continue Ferailleur work: fraise firmwares and REPASSE machine

--- Ferailleur/fw/main.c
+++ Ferailleur/fw/main.c
@@ -11,9 +11,9 @@
 
 t_delay mainDelay;
 
-/*#define LED0 K4
-#define LED1 K11
-#define LED2 K12*/
+#define LED0 K4
+//#define LED1 K11
+//#define LED2 K12*/
 
 void setup(void) {	
 //----------- Setup ----------------
@@ -23,15 +23,18 @@ void setup(void) {
 	pinModeDigitalOut(LED); 	// set the LED pin mode to digital out
 	delayStart(mainDelay, 5000); 	// init the mainDelay to 5 ms
 
-	/*pinModeDigitalOut(LED0); 	// set the LED pin mode to digital out
+	pinModeDigitalOut(LED0); 	// set the LED pin mode to digital out
 	digitalClear(LED0);
-	pinModeDigitalOut(LED1); 	// set the LED pin mode to digital out
+	/*pinModeDigitalOut(LED1); 	// set the LED pin mode to digital out
 	digitalClear(LED1);
 	pinModeDigitalOut(LED2); 	// set the LED pin mode to digital out
 	digitalClear(LED2);*/
 
 //----------- Analog setup ----------------
 	analogInit();		// init analog module
+	analogSelect(0,K5);	
+	analogSelect(1,K6);	
+	analogSelect(2,K7);	
 
 	switchInit();
 	INTCON2bits.RBPU = 0; // enable pullups on PORTB
@@ -45,7 +48,7 @@ void setup(void) {
 	switchSelect(6,K1);
 	switchSelect(7,K2);
 	switchSelect(8,K3);
-
+	switchSelect(9,K12);
 }
 
 void loop() {
@@ -56,7 +59,7 @@ void loop() {
 	if(delayFinished(mainDelay)) // when mainDelay triggers :
 	{
 		delayStart(mainDelay, 5000); 	// re-init mainDelay
-		if(!switchSend()) analogSend();		// send analog channels that changed
+		if(!switchSend()) analogSend();		// send switches channels that changed, if none send analogs that changed
 	}
 }
 
@@ -71,13 +74,13 @@ void fraiseReceiveChar() // receive text
 		c=fraiseGetChar();
 		digitalWrite(LED, c!='0');		
 	}
-	/*else if(c=='l'){		//switch LED on/off 
+	else if(c=='l'){		//switch LED on/off 
 		c=fraiseGetChar();
 		c2=fraiseGetChar();
 		if(c == '0') digitalWrite(LED0, c2 != '0');		
-		else if(c == '1') digitalWrite(LED1, c2 != '0');		
-		else if(c == '2') digitalWrite(LED2, c2 != '0');		
-	}*/
+		/*else if(c == '1') digitalWrite(LED1, c2 != '0');		
+		else if(c == '2') digitalWrite(LED2, c2 != '0'); */
+	}
 	else if(c=='E') { 	// echo text (send it back to host)
 		printf("C");
 		c = fraiseGetLen(); 			// get length of current packet
